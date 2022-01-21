4ba0d32276ce2cf16e3decf1d3e1ec3d6304d88f
Albin Daniel <albin.daniel@royalcyber.com>
2022-01-21 12:12:08 +0530

    Context Provider in TODOLIST

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { ThemeContext } from '../context/ThemeContext';
 class TodoList extends React.Component{
+    static contextType =  ThemeContext;
     render(){
+        const { isDarkTheme, darkTheme, lightTheme} = this.context;
+
+        const theme = isDarkTheme ? darkTheme : lightTheme;
         return(
-            <div className='ui list'>
+            <div style={{ background: theme.background, color: theme.text, height: '140px '}} >
                 <p className='iten'>Trip</p>
                 <p className='iten'>Study</p>
                 <p className='iten'>Pray</p>
