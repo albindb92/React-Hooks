f625c610ba3e46e0daeca8c862c15335a392de06
albindb92 <albindb92@users.noreply.github.com>
2022-01-22 14:55:51 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
 //Context type can be used in class component but not in functional
+//Context consumer can be used in both class and functional
 class Navbar extends React.Component {
-    static contextType =  ThemeContext;
+  
     render() {
-        console.log(this.context);
-        //Destructing of Json Object;
-        const { isDarkTheme, darkTheme, lightTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
+
         return (
-            <nav style={{ background: theme.background, color: theme.text, height: '120px '}}>
-            <h2 style={{ textAlign : 'center'}}>
-            My test 
-            </h2>
-            <div className = 'ui three buttons'> </div>
-            <button className = 'ui button'> Overview </button>
-            <button className = 'ui button'> Contact </button>
-            <button className = 'ui button'> Support </button>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const { isDarkTheme, darkTheme, lightTheme} = context;
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+               return(
+               <nav style={{ background: theme.background, color: theme.text, height: '120px '}}>
+                <h2 style={{ textAlign : 'center'}}>
+                My test 
+                </h2>
+                <div className = 'ui three buttons'> </div>
+                <button className = 'ui button'> Overview </button>
+                <button className = 'ui button'> Contact </button>
+                <button className = 'ui button'> Support </button>
+                </nav>
+                )
+            }}
+            </ThemeContext.Consumer>
+
         )
     }
 }
